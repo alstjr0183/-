2b6c149de3b208dfff6f5b2785f7f9f910976f70
minsuk <alstjr0183@naver.com>
2022-09-13 21:37:42 +0900

    숫자 카드게임 계획, 풀이 수정

--- 문제/숫자 카드 게임.test.js
+++ 문제/숫자 카드 게임.test.js
@@ -1,30 +1,53 @@
-function solution (row, array) {
+// for문 sort 사용
+function solution(Matrix, array) {
+    const [row, column] = Matrix;
+
     let card = []
 
-    for(let i =0; i<row[1]; i++){
-        if(i === 0){
-            card.push(array.slice(0,row[1]))
+    for (let i = 0; i < row; i++) {
+        if (i === 0) {
+            card.push(array.slice(0, column))
         } else {
-            card.push(array.slice(row[1] * i , row[1] * (i + 1)))
+            card.push(array.slice(column * i, column * (i + 1)))
         }
-        
     }
 
-    const result = card.map((item)=>{
-        return item.sort((a,b)=>a-b)
-    }).map((s)=> s[0])
+    const result = card.map((item) => {
+        return item.sort((a, b) => a - b)
+    }).map((s) => s[0])
 
-    minNum = result.sort((a,b)=>b-a)
-    
+    minNum = result.sort((a, b) => b - a)
+
     return minNum[0]
 }
 
- describe('숫자 카드게임',()=>{
-    it('케이스1 실행',()=>{
-        expect(solution([3,3],[3,1,2,4,1,4,2,2,2])).toBe(2)
+// 재귀함수 Math() 사용
+function solution2(Matrix, array, count = 0, card = []) {
+    const [row, column] = Matrix;
+
+    if (count === row) {
+        const result = card.map((item) => Math.min(...item))
+        minNum = Math.max(...result)
+        return minNum
+    }
+
+    if (count === 0) {
+        card.push(array.slice(0, column))
+        return solution(Matrix, array, count + 1, card)
+    } else {
+        card.push(array.slice(column * count, column * (count + 1)))
+        return solution(Matrix, array, count + 1, card)
+    }
+}
+
+describe('숫자 카드게임', () => {
+    it('solution1 실행', () => {
+        expect(solution([3, 3], [3, 1, 2, 4, 1, 4, 2, 2, 2])).toBe(2)
+        expect(solution([2, 4], [7, 3, 1, 8, 3, 3, 3, 4])).toBe(3)
     })
 
-    it('케이스2 실행',()=>{
-        expect(solution([2,4],[7,3,1,8,3,3,3,4])).toBe(3)
+    it('solution2 실행', () => {
+        expect(solution([3, 3], [3, 1, 2, 4, 1, 4, 2, 2, 2])).toBe(2)
+        expect(solution([2, 4], [7, 3, 1, 8, 3, 3, 3, 4])).toBe(3)
     })
 })
