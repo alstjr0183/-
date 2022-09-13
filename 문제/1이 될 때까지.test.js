757f6cd8e9d3e5470421b5441363dbbcc1bb9885
minsuk <alstjr0183@naver.com>
2022-09-13 09:41:43 +0900

    1일 될때까지 변수명 수정

--- 문제/1이 될 때까지.test.js
+++ 문제/1이 될 때까지.test.js
@@ -1,18 +1,18 @@
 function solution ( value, count ) {
     let startValue = value[0]
-    const division = value[1]
+    const divisionValue = value[1]
 
     if(startValue === 1 ){
         return count;
     }
 
-    if(startValue % division === 0){
-        startValue = startValue/division
+    if(startValue % divisionValue === 0){
+        startValue = startValue/divisionValue
     } else {
         startValue = startValue - 1
     }
 
-    return solution([startValue, division] , count + 1)
+    return solution([startValue, divisionValue] , count + 1)
 }
 
  describe('1이 될때까지',()=>{
