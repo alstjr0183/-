fc1019a5585bac1ce0d478cc0bf4a3e396c0f4f3
minsuk <alstjr0183@naver.com>
2022-09-16 13:02:52 +0900

    볼링공고르기 뷸

---
+++ 문제/11/볼링공 고르기.test.js
@@ -0,0 +1,19 @@
+function solution1(N, M, array) {
+    array.sort()
+
+    const [firstNumber] = array;
+
+    const answer = 0
+
+    // for (let i = 0; i < N - 1; i++) {
+    //     for(let ii = 0+; ii<)
+    // }
+
+
+}
+
+describe('', () => {
+    it('solution1 실행', () => {
+        expect(solution1(5, 3, [1, 3, 2, 3, 2])).toBe(8)
+    })
+})
