19497b52065aecb4155a95506549535469a230f9
minsuk <alstjr0183@naver.com>
2022-09-16 10:23:50 +0900

    만들수없는 금액 재귀 , map 풀이추가

--- 문제/11/만들수없는금액.test.js
+++ 문제/11/만들수없는금액.test.js
@@ -1,20 +1,59 @@
+// for
 function solution1(money) {
     let n = money.sort((a, b) => a - b)
 
-    let num = 1
+    let target = 1
 
     for (let i = 0; n.length; i++) {
-        if (num < n[i]) {
+        if (target < n[i]) {
             break
         }
-        num += n[i]
+        target += n[i]
     }
 
-    return num
+    return target
+}
+
+// 재귀함수
+function solution2(money, target = 1, count = 0) {
+    let n = money.sort((a, b) => a - b)
+
+
+    if (target < n[count]) {
+        return target
+    }
+
+    return solution2(money, target += n[count], count + 1)
+}
+
+// Map 
+function solution3(money) {
+    let n = money.sort((a, b) => a - b)
+
+    let target = 1
+
+    n.map((item) => {
+        if (target < item) {
+            console.log(target)
+            return target
+        } else {
+            target = target + item
+        }
+    })
+
+    return target
 }
 
 describe('만들수없는금액', () => {
     it('solution1 실행', () => {
         expect(solution1([3, 2, 1, 1, 9])).toBe(8)
+    })
+
+    it('solution2 실행', () => {
+        expect(solution2([3, 2, 1, 1, 9])).toBe(8)
+    })
+
+    it('solution3 실행', () => {
+        expect(solution3([3, 2, 1, 1, 9])).toBe(8)
     })
 })
