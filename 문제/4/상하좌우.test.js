46448cf73aa3963d7a9d4244dd9bf4c5082dd0f0
minsuk <alstjr0183@naver.com>
2022-09-19 21:03:39 +0900

    상하좌우 풀이

--- 문제/4/상하좌우.test.js
+++ 문제/4/상하좌우.test.js
@@ -1,38 +1,34 @@
 function solution1(n, plan) {
-    let [x, y] = [1, 1]
+    let x = 1,
+        y = 1
 
-    for (let i = 0; i < plan.length + 1; i++) {
-        let check = directionCheck(`${x}${y}`, n)
-        console.log(x, y)
-        if (check.includes('R') && plan[i] === 'R') y = y + 1
-        if (check.includes('L') && plan[i] === 'L') y = y - 1
-        if (check.includes('U') && plan[i] === 'U') x = x - 1
-        if (check.includes('D') && plan[i] === 'D') x = x + 1
+    for (let i = 0; i < plan.length; i++) {
+        let saveX = x
+        let saveY = y
+
+        if (plan[i] === 'R') x = x + 1
+        if (plan[i] === 'L') x = x - 1
+        if (plan[i] === 'U') y = y - 1
+        if (plan[i] === 'D') y = y + 1
+
+        if (y <= 0 || y > n) {
+            y = saveY
+        }
+        if (x <= 0 || x > n) {
+            x = saveX
+        }
     }
 
-    return `${x} ${y}`
+    return `${y} ${x}`
 }
 
-function directionCheck(nowxy, n) {
-    if (nowxy === '11') return 'RD'
-    if (nowxy === `1${n}`) return 'LD'
-    if (nowxy === `${n}1`) return 'UR'
-    if (nowxy === `${n}${n}`) return 'UL'
 
-    for (let i = 2; i < n; i++) {
-        if (nowxy === `1${i}`) return 'RDL'
-        if (nowxy === `${i}1`) return 'ULD'
-        if (nowxy === `${n}${i}`) return 'LUR'
-        if (nowxy === `${i}${n}`) return 'ULD'
-    }
-
-    return 'RULD'
-}
 
 
 describe('상하좌우', () => {
     it('solution1을 실행한다', () => {
         expect(solution1(5, ['R', 'R', 'R', 'U', 'D', 'D'])).toBe('3 4')
-        // expect(solution1(5, ['D', 'D', 'D', 'L', 'R', 'R'])).toBe('4 3')
+        expect(solution1(5, ['D', 'D', 'D', 'L', 'R', 'R'])).toBe('4 3')
+        expect(solution1(5, ['L', 'L', 'D', 'U', 'D', 'D'])).toBe('3 1')
     })
 })
