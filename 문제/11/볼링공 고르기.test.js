2cec3a2ce3d3486598d29bb39496dd5a09ab7162
minsuk <alstjr0183@naver.com>
2022-09-19 11:01:47 +0900

    볼링공 고르기 - for 풀이

--- 문제/11/볼링공 고르기.test.js
+++ 문제/11/볼링공 고르기.test.js
@@ -1,20 +1,25 @@
-function solution1(N, M, array) {
+// for
+function solution1(array) {
+    const N = array.length
+
     array.sort()
 
-    const [firstNumber] = array;
+    let result = 0
 
-    const answer = 0
+    for (let i = 0; i < N - 1; i++) {
+        for (let d = i + 1; d < N; d++) {
+            if (array[i] !== array[d]) {
+                result = result + 1
+            }
+        }
+    }
 
-
-    // for (let i = 0; i < N - 1; i++) {
-    //     for(let ii = 0+; ii<)
-    // }
-
-
+    return result;
 }
 
 describe('', () => {
     it('solution1 실행', () => {
-        expect(solution1(5, 3, [1, 3, 2, 3, 2])).toBe(8)
+        expect(solution1([1, 3, 2, 3, 2])).toBe(8)
+        expect(solution1([1, 5, 4, 3, 2, 4, 5, 2])).toBe(25)
     })
 })
