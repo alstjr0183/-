dff1260edd612a6524ffc6dffa5070b5e345b59a
minsuk <alstjr0183@naver.com>
2022-09-19 23:31:28 +0900

    게임개발 계획중

---
+++ 문제/4/게임개발.test.js
@@ -0,0 +1,9 @@
+function solution1(world, coordinate, direction) {
+
+}
+
+describe('', () => {
+    it('solution1 실행', () => {
+        expect(solution1([[1, 1, 1, 1], [1, 0, 0, 1], [1, 1, 0, 1], [1, 1, 1, 1], [1, 1], 0])).toBe(3)
+    })
+})
